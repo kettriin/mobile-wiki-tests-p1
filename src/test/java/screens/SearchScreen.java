c61775bf3a1fcaf45f80bf9cedc29af1bcfb7f3a
kettriin <e.shch1904@gmail.com>
2025-12-31 18:42:18 +0300

    mobile wiki tests review fixes x2

--- src/test/java/screens/SearchScreen.java
+++ src/test/java/screens/SearchScreen.java
@@ -1,30 +1,27 @@
-package pages;
+package screens;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 
-public class SearchPage {
+public class SearchScreen {
 
     private final SelenideElement fakeSearchInput = $(accessibilityId("Search Wikipedia"));
     private final SelenideElement trueSearchInput = $(id("org.wikipedia.alpha:id/search_src_text"));
     private final ElementsCollection resultsList =
             $$(id("org.wikipedia.alpha:id/page_list_item_title"));
-    private final SelenideElement articleDescription =
-            $(id("org.wikipedia.alpha:id/pcs-edit-section-title-description"));
     private final SelenideElement articleError = $(id("org.wikipedia.alpha:id/page_error"));
 
 
     @Step("Сделать поиск активным")
-    public SearchPage searchInputActivate() {
+    public SearchScreen searchInputActivate() {
         fakeSearchInput.click();
         return this;
     }
@@ -35,7 +32,7 @@ public class SearchPage {
     }
 
     @Step("Отображаются результаты поиска")
-    public SearchPage resultListNotEmpty() {
+    public SearchScreen resultListNotEmpty() {
         resultsList.shouldHave(sizeGreaterThan(0));
         return this;
     }
@@ -43,11 +40,6 @@ public class SearchPage {
     @Step("Выбрать первю статью")
     public void clickFirstResult() {
         resultsList.first().click();
-    }
-
-    @Step("Описание открытой статьи соответствует поисковому запросу")
-    public void descriptionMatchesQuery(String queryDescription) {
-        articleDescription.shouldHave(text(queryDescription));
     }
 
     @Step("При открытии статьи отображается ошибка")
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,31 +1,31 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
-import pages.SearchPage;
+import screens.SearchScreen;
 
 import static io.qameta.allure.Allure.step;
 
 public class SearchTests extends TestBase {
 
-    SearchPage searchPage = new SearchPage();
+    SearchScreen searchScreen = new SearchScreen();
     String query = "Appium";
 
     @Test
     void successfullSearchTest() {
-        step("Ввести поисковый запрос", () -> searchPage.searchInputActivate().inputSearchQuery(query));
+        step("Ввести поисковый запрос", () -> searchScreen.searchInputActivate().inputSearchQuery(query));
         step("По запросу найдены результаты", () -> {
-            searchPage.resultListNotEmpty();
+            searchScreen.resultListNotEmpty();
         });
     }
 
     @Test
     void negativeSearchArticleOpeningTest() {
 
-        step("Ввести поисковый запрос", () -> searchPage.searchInputActivate().inputSearchQuery(query));
+        step("Ввести поисковый запрос", () -> searchScreen.searchInputActivate().inputSearchQuery(query));
         step("По запросу найдены результаты", () -> {
-            searchPage.resultListNotEmpty();
+            searchScreen.resultListNotEmpty();
         });
-        step("Выбрать статью из списка результатов", () -> searchPage.clickFirstResult());
-        step("Отображается ошибка при открытии статьи", () -> searchPage.errorWithArticle());
+        step("Выбрать статью из списка результатов", () -> searchScreen.clickFirstResult());
+        step("Отображается ошибка при открытии статьи", () -> searchScreen.errorWithArticle());
     }
 }
